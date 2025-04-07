64ddbb9ce62388a950d7b5ccf9251eed5c986543
Daniele Costa <santoscostadaniele@gmail.com>
2025-04-07 19:49:00 -0300

    diretórios, arquivos e API pizza

---
+++ DotNetDirectory/Program.cs
@@ -0,0 +1,88 @@
+﻿// // achar o diretório
+var currentDirectory = Directory.GetCurrentDirectory();
+Console.WriteLine(currentDirectory);
+// // diretório existente
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+// // criar diretório
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir", "newDir");
+Directory.CreateDirectory(salesTotalDir);
+
+// criar arquivos
+File.WriteAllText(Path.Combine(salesTotalDir, "greeting.txt"), "Hello");
+
+// verificar se o diretório existe
+bool doesDirectoryExist = Directory.Exists(salesTotalDir);
+Console.WriteLine(doesDirectoryExist);
+
+// // colocar dentro da pasta stores
+var salesFiles = FindFiles(storesDirectory);
+
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+
+// // criar arquivo txt
+// File.WriteAllText(Path.Combine(salesTotalDir, "total.txt"), String.Empty);
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        // verifica a versão do arquivo
+        var extension = Path.GetExtension(file);
+        if (extension == ".txt")
+        {
+            salesFiles.Add(file);
+        }
+    }
+    return salesFiles;
+}
+
+// consultar diretório
+IEnumerable<string> listOfDirectories = Directory.EnumerateDirectories("salesTotalDir");
+
+foreach (var dir in listOfDirectories)
+{
+    Console.WriteLine(dir);
+}
+
+// consultar arquivos
+IEnumerable<string> files = Directory.EnumerateFiles("salesTotalDir");
+
+foreach (var file in files)
+{
+    Console.WriteLine(file);
+}
+
+IEnumerable<string> allFilesInAllFolders = Directory.EnumerateFiles("salesTotalDir", "*.txt", SearchOption.AllDirectories);
+
+foreach (var file in allFilesInAllFolders)
+{
+    Console.WriteLine(file);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "stores", "201"));
+
+// criar arquivo
+// File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "greeting.txt", "Hello World!");)
+
+// verificar se o diretório existe
+// bool doesDirectoryExist = Directory.Exists(filePath);
--- README.md
+++ README.md
@@ -1,7 +1,38 @@
 <p>Criar projeto no terminal</p>
 
 `dotnet new console`
+`dotnet new console -f net8.0 -n DotNetDirectory`
 
 <p>Executar programa</p>
 
 `dotnet run`
+
+<p>System.Text.Json</p>
+
+`dotnet add package System.Text.Json`
+`using Newtonsoft.Json;`
+
+<p>Criar API Web básica</p>
+
+`dotnet new webapi -controllers -f net8.0`
+
+<p>Consultar pelo prompt</p>
+
+`dotnet tool install -g Microsoft.dotnet-httprepl`
+`httprepl https://localhost:{PORT}`
+`ls`
+`get`
+`delete`
+`exit`
+
+<p>Criar pasta</p>
+
+`mkdir Pasta`
+
+<p>Post terminal</p>
+
+`post -c "{"name":"Hawaii", "isGlutenFree":false}"`
+
+<p>Put terminal</p>
+
+`put 3 -c  "{"id": 3, "name":"Hawaiian", "isGlutenFree":false}"`
